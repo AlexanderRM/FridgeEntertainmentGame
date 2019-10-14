bed456f492c82e45c250c10d634e31450934c319
Dimitri Lambrou <dimitrilambrou1@gmail.com>
2019-10-14 13:50:00 +1100

    added assets

---
+++ FridgeEntertainmentGame/Assets/Scripts/ColorLerper.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorLerper : MonoBehaviour
+{
+    public float speed = 1.0f;
+    public Color startColor;
+    public Color endColor;
+    public string objName;
+    private bool coffee = false;
+    float startTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(coffee == true)
+        {
+            float t = (Time.time - startTime) * speed;
+            GetComponent<Renderer>().material.color = Color.Lerp(startColor, endColor, t);
+        }
+    }
+
+    void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.name == objName && startTime == 0)
+        {
+            coffee = true;
+
+            startTime = Time.time;
+        }
+    }
+}
---
+++ FridgeEntertainmentGame/Assets/Scripts/TankController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankController : MonoBehaviour
+{
+    public float speed = 3.0f;
+    public float rotationSpeed = 90.0f;
+
+    private void Update()
+    {
+        float rotateTank = Input.GetAxis("Horizontal");
+        float moveTank = Input.GetAxis("Vertical");
+
+        GetComponent<Rigidbody>().velocity = transform.forward * speed * moveTank;
+
+        transform.Rotate(Vector3.up * rotationSpeed * rotateTank * Time.deltaTime);
+    }
+}
