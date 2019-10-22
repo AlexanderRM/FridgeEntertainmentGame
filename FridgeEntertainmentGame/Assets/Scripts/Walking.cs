cc48e24f56e2a3f455dac58b2b866e4a1fde9c95
BrokenPiG <david.eammano@yahoo.com.au>
2019-10-22 15:27:52 +1100

    Walking Animation

---
+++ FridgeEntertainmentGame/Assets/Scripts/Walking.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Walking : MonoBehaviour
+{
+
+    private Animator anim;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>(); 
+    }
+    
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+        {
+            anim.SetBool("NotWalking", true);
+        }
+        else
+        {
+            anim.SetBool("NotWalking", false);
+        }
+    }
+}
