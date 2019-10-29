36ebe00c457f6019a42d4f163c3b152dac46e79a
Dimitri Lambrou <dimitrilambrou1@gmail.com>
2019-10-30 10:05:34 +1100

    Added Light

---
+++ FridgeEntertainmentGame/Assets/Scripts/LightToggle.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightToggle : MonoBehaviour
+{
+    new Light light;
+    public Person personScript;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        light = GetComponent<Light>();
+        personScript = GetComponent<Person>();
+        light.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(personScript.coffee == true)
+        {
+            light.enabled = true;
+        }
+    }
+}
