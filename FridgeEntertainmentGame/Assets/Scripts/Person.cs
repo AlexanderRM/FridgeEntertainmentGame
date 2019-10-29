6b32d3491779b548ab9fb60ec704b5b6118367ba
Dimitri Lambrou <dimitrilambrou1@gmail.com>
2019-10-29 15:08:06 +1100

    Update Person.cs

--- FridgeEntertainmentGame/Assets/Scripts/Person.cs
+++ FridgeEntertainmentGame/Assets/Scripts/Person.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using VIDE_Data;
 
 public class Person : MonoBehaviour
 {
@@ -11,10 +12,22 @@ public class Person : MonoBehaviour
     public bool active = false;
     public bool solved = false;
     public Beans bean;
+    public bool coffee = false;
+
+    // Dialogue
+    public int chatStart;
+    public int spokenAgain;
+    public int activeUnsolved;
+    public int notActive;
+    public int riddleSolved;
+    public int coffeeGive;
+    public int coffeeGiven;
+
     private TankController playerScript;
     private ParticleSystem particle;
+    private VIDE_Assign vide;
+    private bool chat = false;
 
-    public bool coffee = false;
 
     // Start is called before the first frame update
     void Start()
@@ -22,6 +35,7 @@ public class Person : MonoBehaviour
         // Set values
         playerScript = player.GetComponent<TankController>();
         particle = particleEffect.GetComponent<ParticleSystem>();
+        vide = GetComponent<VIDE_Assign>();
 
         particle.Stop();
     }
@@ -45,18 +59,47 @@ public class Person : MonoBehaviour
 
     void OnTriggerStay(Collider collider)
     {
+        // check if object collision matches
         if (collider.gameObject.name == colTargetName)
         {
+            // if coffee has been given
+            if(coffee == true)
+            {
+                vide.overrideStartNode = coffeeGiven;
+                return;
+            }
+
+            if(bean.solved == true)
+            {
+                vide.overrideStartNode = riddleSolved;
+            }
+
+            // check if player has coffee
             if (playerScript.coffee == true && Input.GetKeyDown(KeyCode.Return) == true)
             {
                 coffee = true;
+                vide.overrideStartNode = coffeeGive;
             }
+            // if active give riddle
             else if (active == true && Input.GetKeyDown(KeyCode.Return) == true)
             {
-                // Send Riddle
+                vide.overrideStartNode = chatStart;
 
+                // If talked to before give other dialogue
+                if(chat == true)
+                {
+                    vide.overrideStartNode = spokenAgain;
+                }
+
+                // set bool for first chat
+                chat = true;
                 // Set bean active
                 bean.active = true;
+            }
+            // active is false
+            else if (active == false)
+            {
+                vide.overrideStartNode = notActive;
             }
         }
     }
