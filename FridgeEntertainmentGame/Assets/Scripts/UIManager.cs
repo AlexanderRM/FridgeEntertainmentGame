3c6c89dc4fc87f17503a52748bf0bf9448ad3173
AlexanderRM <55731002+AlexanderRM@users.noreply.github.com>
2019-11-04 16:51:29 +1100

    Beans

--- FridgeEntertainmentGame/Assets/Scripts/UIManager.cs
+++ FridgeEntertainmentGame/Assets/Scripts/UIManager.cs
@@ -32,7 +32,7 @@ public class UIManager : MonoBehaviour
 
     void Awake()
     {
-        // VD.LoadDialogues(); //Load all dialogues to memory so that we dont spend time doing so later
+        VD.LoadDialogues(); //Load all dialogues to memory so that we dont spend time doing so later
         //An alternative to this can be preloading dialogues from the VIDE_Assign component!
 
         //Loads the saved state of VIDE_Assigns and dialogues.
